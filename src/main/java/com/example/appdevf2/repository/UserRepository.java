96c6b8766c336ae903c7c41a2e1719ac2be02fbd
jones21312321313213 <skinnyjones212121@gmail.com>
2025-10-17 11:40:21 +0800

    added things in user repo and saervice

--- src/main/java/com/example/appdevf2/repository/UserRepository.java
+++ src/main/java/com/example/appdevf2/repository/UserRepository.java
@@ -1,5 +1,10 @@
 package com.example.appdevf2.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.example.appdevf2.entity.UserEntity;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity,Integer>{
 }
--- src/main/java/com/example/appdevf2/service/UserService.java
+++ src/main/java/com/example/appdevf2/service/UserService.java
@@ -1,5 +1,58 @@
 package com.example.appdevf2.service;
 
-public class UserService {
+import java.util.List;
+import java.util.NoSuchElementException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.appdevf2.entity.UserEntity;
+import com.example.appdevf2.repository.UserRepository;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository urepo;
+    
+    //C -  Create or insert user record in tbluser
+    public UserEntity insertUser(UserEntity user){
+        return urepo.save(user);
+    }
+
+    //R - read all user records in tbluser
+    public List<UserEntity> getAllUsers(){
+        return urepo.findAll();
+    }
+
+
+    //U - update a user
+    @SuppressWarnings("finally")
+    public UserEntity updateUser(int uid, UserEntity newUserDetails){
+        UserEntity u = new UserEntity();
+        try{
+            //1) Search the id number of the user that will be updated
+            u = urepo.findById(uid).get();
+            //2) upddate the record
+            u.setUsername(newUserDetails.getUsername());
+            u.setEmail(newUserDetails.getEmail());
+            u.setPassword(newUserDetails.getPassword());
+        }catch(NoSuchElementException e){
+            throw new NoSuchElementException("User: " +  uid + " is not found");
+        }finally{
+            return urepo.save(u);
+        }
+    }
+
+
+    // D - delete a user
+    public String deleteUser(int uid){
+        String msg = "";
+        if(urepo.existsById(uid)){
+            urepo.deleteById(uid);
+            msg = "User: " + uid + " is successfully deleted!";
+        }else{
+            msg = "User: " + uid + " does not exist.";
+        }
+        return msg;
+    }
 }
