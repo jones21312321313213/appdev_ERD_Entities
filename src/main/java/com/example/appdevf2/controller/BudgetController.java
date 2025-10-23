a8ff0a0c637db94b0637feb76857fcb9c88f15ef
jones21312321313213 <skinnyjones212121@gmail.com>
2025-10-23 22:03:02 +0800

    a

--- src/main/java/com/example/appdevf2/controller/BudgetController.java
+++ src/main/java/com/example/appdevf2/controller/BudgetController.java
@@ -1,13 +1,21 @@
 package com.example.appdevf2.controller;
 
-import com.example.appdevf2.entity.BudgetEntity;
-import com.example.appdevf2.service.BudgetService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.example.appdevf2.entity.BudgetEntity;
+import com.example.appdevf2.service.BudgetService;
 
 @RestController
 @RequestMapping("/api/budgets")
@@ -37,6 +45,11 @@ public class BudgetController {
                 .map(budget -> new ResponseEntity<>(budget, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
+    
+
+
+
+
 
     // PUT: Update an existing Budget
     @PutMapping("/{id}")
