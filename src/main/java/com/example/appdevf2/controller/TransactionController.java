a1b5254a9b42e8560367e7b984a01aaf487f205b
jones21312321313213 <skinnyjones212121@gmail.com>
2025-10-23 22:00:45 +0800

    added getByID

--- src/main/java/com/example/appdevf2/controller/TransactionController.java
+++ src/main/java/com/example/appdevf2/controller/TransactionController.java
@@ -3,6 +3,7 @@ package com.example.appdevf2.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,6 +36,19 @@ public class TransactionController {
         return tserv.getAllTransactions();
     }
 
+
+    @GetMapping("/getTransaction/{id}")
+    public ResponseEntity<TransactionEntity> getTransactionById(@PathVariable int id){
+          TransactionEntity transaction = tserv.getTransactionById(id); 
+
+          if(transaction != null){
+            return ResponseEntity.ok(transaction);
+          }else{
+            return ResponseEntity.notFound().build();
+          }
+    }
+
+    
     // U - update a transaction
     @PutMapping("/updateTransaction")
     public TransactionEntity updateTransaction(@RequestParam int tid, @RequestBody TransactionEntity newTransactionDetails) {
